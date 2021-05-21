0b64cf59659423b912573501d7357b80cb932ab1
blast-xyz <84601145+blast-xyz@users.noreply.github.com>
2021-05-22 00:18:36 +0300

    script.sh
    update eden scriptim

---
+++ script.sh
@@ -0,0 +1,8 @@
+read -p "gereksinimler yüklensin mi? kabul etmek için enter, kabul etmemek için ctrl + c yazın! = "
+clear
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt dist-upgrade
+clear
+sleep 3
+echo -e "sistem başarıyla güncellendi"
